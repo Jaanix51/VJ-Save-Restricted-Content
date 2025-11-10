fe741dc65dd9f3a6c6e25b10e41b069c578691f2
GodZilla <144112470+Jaanix51@users.noreply.github.com>
2025-11-10 21:15:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,21 +8,21 @@ if LOGIN_SYSTEM == False:
     STRING_SESSION = os.environ.get("STRING_SESSION", "")
 
 # Bot token @Botfather
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("8517968392:AAGZk9F1jPRxfV5WXLk9CbLrq78Zcg9qR94", "")
 
 # Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("22951848", ""))
 
 # Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("178201e3c0577efb4c1acba9863ed669", "")
 
 # Your Owner / Admin Id For Broadcast 
-ADMINS = int(os.environ.get("ADMINS", "6073523936"))
+ADMINS = int(os.environ.get("ADMINS", "5751472348"))
 
 # Your Mongodb Database Url
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_URI = os.environ.get("DB_URI", "") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_NAME = os.environ.get("DB_NAME", "vjsavecontentbot")
+DB_URI = os.environ.get("mongodb+srv://nehalsingh20242024_db_user:7CPP7raHTop05HR2@cluster0.zv4n0if.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", "") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_NAME = os.environ.get("nehalsingh20242024", "vjsavecontentbot")
 
 # If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
 ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
